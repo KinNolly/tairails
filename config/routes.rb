125fb845f6b8647756c295aaecd2f73f18b301bc
Nguyễn Hữu Trường Tài <truongtai280923@gmail.com>
2025-01-10 10:52:51 +0700

    update notification

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resource :unsubscribe, only: [ :show ]
   resource :session
   resources :passwords, param: :token
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
   
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
